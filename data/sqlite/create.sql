38c670e5ec0a45c075bc7cecc629ce4cba26b412
HamzaMakri <47712580+HamzaMakri@users.noreply.github.com>
2020-06-09 22:03:59 +0200

    Base de donnée incsription (create.sql)

--- data/sqlite/create.sql
+++ data/sqlite/create.sql
@@ -13,10 +13,56 @@
 /*
 
 
-Compte (id,nom,prenom,mail,adresse,telephone,statut)
-Inscription(numInscript,sexe,nom,prenom,mail,dateNaiss,Nationalité,Profession,tel,adresse,bénévole,validée,handicap,attestationmedicale,date)
-DemandeSport(numInscript,nomsport)
-Sport(nomsport,competition)
+CREATE TABLE `user`(
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(255) NOT NULL,
+  prenom VARCHAR(255) NOT NULL,
+  dateNaiss DATE NOT NULL,
+  mail VARCHAR(255) NOT NULL,
+  mdp VARCHAR(255) NOT NULL,
+  statut ENUM('adherent','benevole','admin') NOT NULL,
+  numInscript INT NOT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (numInscript) REFERENCES inscription(numInscript)
+);
+
+CREATE TABLE `inscription` (
+  numInscript INT NOT NULL AUTO_INCREMENT,
+  sexe ENUM('h','f') NOT NULL,
+  nom VARCHAR(255) NOT NULL,
+  prenom VARCHAR(255) NOT NULL,
+  mail VARCHAR(255) NOT NULL,
+  dateNaiss DATE NOT NULL,
+  nationalite VARCHAR(255) NOT NULL,
+  profession VARCHAR(255) NOT NULL,
+  tel INT NOT NULL,
+  adresse VARCHAR(255) NOT NULL,
+  statut ENUM('adherent','benevole','admin') NOT NULL,
+  validee BOOLEAN NOT NULL DEFAULT 1,
+  handicap VARCHAR(255),
+  attestationmedicale VARCHAR(255),
+  date DATE DEFAULT CURRENT_DATE,
+
+  PRIMARY KEY (numInscript)
+);
+
+CREATE TABLE `inscriptionSport` (
+  nomsport VARCHAR(255) NOT NULL,
+  numInscript int,
+
+  PRIMARY KEY (numInscript,nomsport),
+  FOREIGN KEY (numInscript) REFERENCES inscription(numInscript)
+);
+
+
+
+
+
+
+
+
+
 
 
 
