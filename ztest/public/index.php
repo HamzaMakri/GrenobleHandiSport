c577cd87867efd0f2550149687d080fb3423b9b9
you12234 <44065736+you12234@users.noreply.github.com>
2020-06-12 06:51:07 +0200

    pbm edit et ajout d'evenements résolus

--- ztest/public/index.php
+++ ztest/public/index.php
@@ -33,6 +33,8 @@ require '../views/header.php';
 
 <div class="calendar">
 
+<?php if (isset($_SESSION['user'])) {
+    if (  $_SESSION['user']->statut == 'admin'): ?>
   <div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
     <h1><?= $month->toString(); ?></h1>
 
@@ -75,19 +77,45 @@ require '../views/header.php';
       <?php endfor; ?>
   </table>
   <?php
-
-if (isset($_SESSION['user'])) {
-  if (  $_SESSION['user']->statut == 'admin'){
     echo'
     <a href="add.php" class="calendar__button">+</a>
     ';
-  }
-}
+    else : ?>
+      <div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
+        <h1><?= $month->toString(); ?></h1>
+        <div>
+          <a href="index.php?month=<?= $month->previousMonth()->month; ?>&year=<?= $month->previousMonth()->year; ?>" class="btn btn-primary">&lt;</a>
+          <a href="index.php?month=<?= $month->nextMonth()->month; ?>&year=<?= $month->nextMonth()->year; ?>" class="btn btn-primary">&gt;</a>
+        </div>
+      </div>
 
-?>
 
-
-  <a href="add.php" class="calendar__button">+</a>
+      <table class="calendar__table calendar__table--<?= $weeks; ?>weeks">
+        <?php for ($i = 0; $i < $weeks; $i++): ?>
+        <tr>
+          <?php
+          foreach($month->days as $k => $day):
+              $date = (clone $start)->modify("+" . ($k + $i * 7) . " days");
+              $eventsForDay = $events[$date->format('Y-m-d')] ?? [];
+              ?>
+          <td class="<?= $month->withinMonth($date) ? '' : 'calendar__othermonth'; ?>">
+            <?php if ($i === 0): ?>
+              <div class="calendar__weekday"><?= $day; ?></div>
+            <?php endif; ?>
+            <div class="calendar__day"><?= $date->format('d'); ?></div>
+            <?php foreach($eventsForDay as $event): ?>
+            <div class="calendar__event">
+              <?= (new DateTime($event['start']))->format('H:i') ?> - <a href="event.php?id=<?= $event['id']; ?>"><?= h($event['name']); ?></a>
+            </div>
+            <?php endforeach; ?>
+          </td>
+          <?php endforeach; ?>
+        </tr>
+        <?php endfor; ?>
+      </table>
+     <?php endif;
+   }
+    ?>
 
 </div>
 
